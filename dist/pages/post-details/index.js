0ae2b8bbd40acae9389987de4b07116079f66b47
d3vi1 <hannanhridoy@gmail.com>
2020-12-10 02:31:24 +0600

    distributed sidebar to each and every page

--- dist/pages/post-details/index.js
+++ dist/pages/post-details/index.js
@@ -1,10 +1,7 @@
-let n1 = document.getElementById('n1')
 let sidebar = document.querySelector('aside')
 let sidebarClose = document.getElementById('sidebarClose')
 let sidebarOpen = document.getElementById('sidebarOpen')
 
-//make first content of post visible
-n1.style.display = 'block'
 //toggling between sidebar and menu
 sidebarClose.addEventListener('click', function () {
     sidebar.style.display = 'none'
